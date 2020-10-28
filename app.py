3af2c9ca393e05e7716316a1dc6ee277a5b9216e
Burke Holland <burkeholland@gmail.com>
2020-10-28 17:12:45 -0500

    Remove code comments - not important to this demo

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ from flask import Flask
 from flask import render_template
 import json
 
-# creates a Flask application, named app
 app = Flask(__name__)
 
-# a route where we will display a welcome message via an HTML template
 @app.route("/")
-def hello():
+def index():
     with open('data/products.json') as f:
         data = json.load(f)
 
     return render_template('index.html', products=data)
 
-# run the application
 if __name__ == "__main__":
     app.run(debug=True)
